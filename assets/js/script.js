eb18472c90f15da3157875081e2fd7a9d43cc7af
Jason Cano <jasoncano0509@gmail.com>
2024-12-28 10:28:16 -0600

    working on the 5 day scheduler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let d = new Date();
 let store = [];
+let main = document.getElementById('main');
 
 
 const handleStorage = async () => {
@@ -18,23 +19,37 @@ handleStorage();
 currentDay.innerText = `${d.toDateString()}, ${d.toLocaleTimeString()}`;
 
 const hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
-main.innerHTML=""
-
-hours.forEach((hour,i) => {
-  let rH = i+9;
-  let cH = d.getHours();
-
+weekdays.forEach(day => {
   main.innerHTML += `
-      <div class="row time-block ${rH<cH ? 'past' : rH>cH ? 'future' : 'present'}">
-        <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
-        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-          <i class="fas fa-save" aria-hidden="true"></i>
-        </button>
-      </div>
-  `
+    <div id=${day}>
+      <h5>${day}</h5>
+    </div>`;
+
+    let div=document.getElementById(day);
+    hours.forEach(hour => {
+        div.innerHTML += `
+          <div>${hour}</div>
+        `
+      
+    });
 });
+
+// hours.forEach((hour,i) => {
+//   let rH = i+9;
+//   let cH = d.getHours();
+
+//   main.innerHTML += `
+//       <div class="row time-block ${rH<cH ? 'past' : rH>cH ? 'future' : 'present'}">
+//         <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
+//         <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+//         <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+//           <i class="fas fa-save" aria-hidden="true"></i>
+//         </button>
+//       </div>
+//   `
+// });
 
 document.onclick = e => {
   if (e.target.classList.contains("saveBtn")) {
