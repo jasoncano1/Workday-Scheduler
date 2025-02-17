118bc355ecdaa71d9e9957b8f7774922e6dbd47c
Jason Cano <jasoncano0509@gmail.com>
2025-02-17 10:00:06 -0600

    worked on login,signup,continue functionality

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
 const path = require('path');
 
-// Home route (could render your existing index.html or another page)
+// GET route for the login page
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'login.html'));
 });
 
-// GET route for the login page
-router.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+// Home route (could render your existing index.html or another page)
+router.get('/workday', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'scheduler.html'));
 });
+
 
 // GET route for the signup page
 router.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+  res.sendFile(path.join(__dirname, '../public', 'signup.html'));
 });
 
 module.exports = router;
