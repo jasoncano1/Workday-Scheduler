ebb0706aa927a9dae9ffcd727c800d207600a1ac
Jason Cano <jasoncano0509@gmail.com>
2025-03-18 10:15:43 -0500

    reformatting code in scheduler

--- public/js/scheduler.js
+++ public/js/scheduler.js
@@ -24,13 +24,167 @@ const h24 = 60 * 60 * 24 * 1000;
 const main = document.getElementById('main');
 const nextWk = document.getElementById('nextWeek');
 const prevWk = document.getElementById('prevWeek');
-const init = (d) => {
+const hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+const getUser = async username => await (await fetch('/api/data', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username })
+  })).json();
+
+const init = async d => {
   localStorage.getItem("username")
     ? (
       username = localStorage.getItem('username'),
-      document.getElementById('username').innerHTML = `Hi ${username}, welcome to your time analysis.`,
+      document.getElementById('username').innerHTML = `Welcome ${username}`,
       document.getElementById('username').style.fontWeight = 'normal'
     )
     : (window.location.href = "/");
+
+  user = await getUser(username);
+  dateTimes = user.tasks.map(obj => obj.date);
+
+  monday = new Date(d.getDay != 1 ? d - (d.getDay() - 1) * 86400000 : d).toDateString().split(' ').join('');
+  tuesday = new Date(d.getDay != 1 ? d - (d.getDay() - 2) * 86400000 : d).toDateString().split(' ').join('');
+  wednesday = new Date(d.getDay != 1 ? d - (d.getDay() - 3) * 86400000 : d).toDateString().split(' ').join('');
+  thursday = new Date(d.getDay != 1 ? d - (d.getDay() - 4) * 86400000 : d).toDateString().split(' ').join('');
+  friday = new Date(d.getDay != 1 ? d - (d.getDay() - 5) * 86400000 : d).toDateString().split(' ').join('');
+
+  weekdays = [monday, tuesday, wednesday, thursday, friday];
+
+  weekdays.forEach((date, i) => {
+
+    dateTimes.forEach(dayTime => {
+      if (dayTime.includes(day)) {
+        totalScheduled += 1;
+
+        if (user.tasks.find(obj => obj.date == dayTime).status == "done") {
+          totalDone += 1
+        };
+      };
+    });
+
+    main.innerHTML += `
+      <section id=${date} class=${new Date() - d > 86400000 ? "past" :
+        new Date() - d < -86400000 ? "future" :
+          i + 1 < d.getDay() ? "past" :
+            i + 1 == d.getDay() ? "present" : "future"
+      }>
+        
+        <h5>${i == 0 ? 'Monday' :
+        i == 1 ? 'Tuesday' :
+          i == 2 ? 'Wednesday' :
+            i == 3 ? 'Thursday' : 'Friday'
+      }</h5>
+      </section>`;
+
+    let div = document.getElementById(date);
+    hours.forEach(hour => {
+      div.innerHTML +=
+        div.classList.contains("past") ?
+          `
+        <div>
+        <h5>${hour}</h5>
+        <input class="_${hour}" disabled />
+        <input class="_${hour}" disabled type="checkbox" />
+        </div>
+        ` :
+          div.classList.contains("future") ?
+            `
+            <div>
+              <h5>${hour}</h5>
+              <input class="_${hour}" onChange="handleChange('${date}_${hour}')" />
+              <input class="_${hour}" disabled type="checkbox" />
+            </div>
+          `:
+            `
+            <div>
+              <h5>${hour}</h5>
+              <input class="_${hour}" onChange="handleChange('${date}_${hour}')" />
+              <input class="_${hour}" onChange="handleChange('${date}_${hour}')" type="checkbox" />
+            </div>
+          `
+    });
+  });
+
+  data = [
+    {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: totalScheduled / totalHours * 100,
+      title: { text: "Quality" },
+      type: "indicator",
+      mode: "gauge+number",
+      delta: { reference: 400 },
+      gauge: {
+        axis: { range: [0, 100] },
+        bar: { color: "red" },
+        steps: [
+          { range: [0, 50], color: "black" },
+          { range: [51, 74], color: "yellow" },
+          { range: [75, 100], color: "green" }
+        ]
+      }
+    }
+  ];
+
+  layout = { width: 300, height: 250, paper_bgcolor: 'transparent' };
+  Plotly.newPlot('chart1', data, layout);
+
+  data2 = [
+    {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: totalDone / (totalScheduled == 0 ? 45 : totalScheduled) * 100,
+      title: { text: "Efficiency" },
+      type: "indicator",
+      mode: "gauge+number",
+      delta: { reference: 400 },
+      gauge: {
+        axis: { range: [0, 100] },
+        bar: { color: "red" },
+        steps: [
+          { range: [0, 50], color: "black" },
+          { range: [51, 74], color: "yellow" },
+          { range: [75, 100], color: "green" }
+        ]
+      }
+    }
+  ];
+
+  layout2 = { width: 300, height: 250, paper_bgcolor: 'transparent' };
+  Plotly.newPlot('chart2', data2, layout2);
+
+  data = [
+    {
+      type: "indicator",
+      mode: "number+gauge+delta",
+      gauge: { shape: "bullet" },
+      delta: { reference: 100 },
+      value: totalScheduled / totalHours * 100,
+      domain: { x: [0, 1], y: [0, 1] },
+      title: { text: "Quality" }
+    }
+  ];
+
+  layout = { width: 350, height: 80, paper_bgcolor: 'transparent', margin: { t: 10, b: 40, l: 120, r: 60 } };
+  Plotly.newPlot('chart1b', data, layout);
+
+
+
+  data2 = [
+    {
+      type: "indicator",
+      mode: "number+gauge+delta",
+      gauge: { shape: "bullet" },
+      delta: { reference: 100 },
+      value: totalDone / (totalScheduled == 0 ? 45 : totalScheduled) * 100,
+      domain: { x: [0, 1], y: [0, 1] },
+      title: { text: "Efficiancy" }
+    }
+  ];
+
+  layout = { width: 350, height: 80, paper_bgcolor: 'transparent', margin: { t: 10, b: 40, l: 120, r: 60 } };
+  Plotly.newPlot('chart2b', data2, layout);
 };
 init(d);
